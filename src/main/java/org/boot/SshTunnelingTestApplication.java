14ff63993b5e579b0d5460bfd8f625a82226d3d7
SonDongGyun <thsehdrbs11@naver.com>
2024-08-14 17:37:13 +0900

    0814 -1

---
+++ src/main/java/org/boot/SshTunnelingTestApplication.java
@@ -0,0 +1,13 @@
+package org.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshTunnelingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SshTunnelingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/boot/SshTunnelingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshTunnelingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
